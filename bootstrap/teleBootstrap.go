6fe8bae87efa5ac31b1f5f2313306ca35a11e457
l_t_yuan <l_t_yuan@163.com>
2023-02-09 18:19:45 +0800

    change

--- bootstrap/teleBootstrap.go
+++ bootstrap/teleBootstrap.go
@@ -10,14 +10,17 @@ import (
 )
 
 func RunTele() {
-	log.Printf("tele token: %s \n", config.LoadConfig().TeleToken)
+	// log.Printf("tele token: %s \n", config.LoadConfig().TeleToken)
 	bot := tbot.New(config.LoadConfig().TeleToken)
 	c := bot.Client()
 
-	bot.HandleMessage(".*yo.*", func(m *tbot.Message) {
-		c.SendChatAction(m.Chat.ID, tbot.ActionTyping)
-		time.Sleep(1 * time.Second)
-		c.SendMessage(m.Chat.ID, "hello!")
+	bot.HandleMessage("*", func(m *tbot.Message) {
+		// c.SendChatAction(m.Chat.ID, tbot.ActionTyping)
+		// time.Sleep(1 * time.Second)
+		// c.SendMessage(m.Chat.ID, "hello!")
+			log.Printf("tele response text: %#v \n", m)
+		// c.SendMessage(m.Chat.ID, hadler.TeleHandler("", m.Chat.ID.String()))
+		c.SendMessage(m.Chat.ID, "ok")
 	})
 	err := bot.Start()
 	if err != nil {
