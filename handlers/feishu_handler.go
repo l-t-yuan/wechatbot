099ade710a27d9d7e9eeb3e2ba0e68ea9ee1521f
l_t_yuan <l_t_yuan@163.com>
2023-03-02 21:32:28 +0800

    add attr

--- handlers/feishu_handler.go
+++ handlers/feishu_handler.go
@@ -3,14 +3,17 @@ package handlers
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/869413421/wechatbot/config"
+	"github.com/869413421/wechatbot/gtp"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	sdkginext "github.com/larksuite/oapi-sdk-gin"
+	lark "github.com/larksuite/oapi-sdk-go/v3"
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
@@ -18,6 +21,7 @@ import (
 
 type FeishuHandler struct {
 	baseHandler func(c *gin.Context)
+	cli         *lark.Client
 }
 type FeishuValidate struct {
 	Challenge string `json:"challenge"`
@@ -25,8 +29,13 @@ type FeishuValidate struct {
 	Type      string `json:"type"`
 }
 
+type FMessageText struct {
+	Text string `json:"text"`
+}
+
 func (f *FeishuHandler) Init() {
 	f.baseHandler = sdkginext.NewEventHandlerFunc(f.GenFeiHandler())
+	f.cli = lark.NewClient(config.LoadConfig().FeiAppId, config.LoadConfig().FeiAppSecret, lark.WithLogReqAtDebug(true), lark.WithLogLevel(larkcore.LogLevelDebug))
 }
 
 func (f *FeishuHandler) GenValidateHandler(c *gin.Context) {
@@ -69,6 +78,34 @@ func (f *FeishuHandler) GenFeiHandler() *dispatcher.EventDispatcher {
 func (f *FeishuHandler) onP2MessageReceiveV1(ctx context.Context, event *larkim.P2MessageReceiveV1) error {
 	fmt.Println(larkcore.Prettify(event))
 	fmt.Println(event.RequestId())
+
+	tenantKey := event.TenantKey()
+	openId := *event.Event.Sender.SenderId.OpenId
+	receiveMessageBody := &FMessageText{}
+	if err := json.Unmarshal([]byte(*event.Event.Message.Content), &receiveMessageBody); err != nil {
+		fmt.Println("================json str 转struct==")
+		fmt.Println(*event.Event.Message.Content)
+	}
+
+	client := gtp.GetChatGptBot()
+	reply, err := client.Chat(receiveMessageBody.Text, "feishu_"+openId)
+	if err != nil {
+		fmt.Println("chat error")
+		reply = "机器人出错了"
+	}
+	// ISV 给指定租户发送消息
+	resp, err := f.cli.Im.Message.Create(context.Background(), larkim.NewCreateMessageReqBuilder().
+		ReceiveIdType(larkim.ReceiveIdTypeOpenId).
+		Body(larkim.NewCreateMessageReqBodyBuilder().
+			MsgType(larkim.MsgTypePost).
+			ReceiveId(openId).
+			Content(reply).
+			Build()).
+		Build(), larkcore.WithTenantKey(tenantKey))
+
+	// 发送结果处理，resp,err
+	fmt.Println(resp, err)
+
 	return nil
 }
 
